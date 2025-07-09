def2eb1189dfc00161f513b19f4a6a81792861ee
sakshiagrawal2711 <94430048+sakshiagrawal2711@users.noreply.github.com>
2025-07-09 14:30:20 +0530

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,7 +1,7 @@
 // src/utils/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000', {
+const socket = io(process.env.REACT_APP_SOCKET_URL || 'https://todo-backend-kehy.onrender.com', {
   transports: ['websocket'],
 });
 
