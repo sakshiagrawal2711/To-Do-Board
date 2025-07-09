ec4e0cfaf07b31e3a68b337043e4e02f0764d731
sakshiagrawal2711 <94430048+sakshiagrawal2711@users.noreply.github.com>
2025-07-09 14:50:04 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://todo-backend-kehy.onrender.com/api",
   withCredentials: true,
 });
 
